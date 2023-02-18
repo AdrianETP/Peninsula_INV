259e2d163fd64a18e4ca0b2116aefbcb10036820
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2023-02-18 17:30:55 -0600

    code

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+const daisyui = require('daisyui')
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+    daisyui:{
+        themes:["garden"]
+    },
+	plugins: [daisyui],
+}
