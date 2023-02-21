f119b0ef18efaa11d83e7007ba322f48104f417e
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2023-02-21 10:43:59 -0600

    https -> http

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: "https://playainversiones.com",
+    site: "http://playainversiones.com",
     base:'/'
 });
